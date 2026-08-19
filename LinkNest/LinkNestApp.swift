fbbaa7f0789fbbab98c2a892a1d0542c964ccf4d
Jitendra <jkumar@edgeevolve.com>
2026-08-19 12:33:20 +0530

    Initial Commit

---
+++ LinkNest/LinkNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkNestApp.swift
+//  LinkNest
+//
+//  Created by Jitendra on 19/08/26.
+//
+
+import SwiftUI
+
+@main
+struct LinkNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
